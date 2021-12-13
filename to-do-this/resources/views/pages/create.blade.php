a5b5ab3aa5e2fb508646361b073a184395c47632
Shouinn <40841116@gm.nfu.edu.tw>
2021-12-13 18:02:38 +0800

    新增路徑 route->post

--- to-do-this/resources/views/pages/create.blade.php
+++ to-do-this/resources/views/pages/create.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.master')
 @section('content')
-    <form action="{{route('store')}}">
+    <form action="{{route('store')}}" method="post">
+        @csrf
     <label>標題</label>
     <input name = "title">
     <label>內容</label>
-    <input name = "cintent">
+    <input name = "content">
     <label>備註</label>
     <input name = "remake">
      <button type="submit">submit</button>
